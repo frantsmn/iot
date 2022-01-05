ce4f382966cd33afeda37d1d44f883482e2bfdaf
Frants Mitskun <frants.mitskun@gmail.com>
2022-01-05 19:51:26 +0300

    добавляет действия по расписанию

--- app.ts
+++ app.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import auth from './auth'
 import router from './router'
 import error from './error'
+import Scheduler from './scheduler';
 
 const app = express();
 const port = 3001;
@@ -11,3 +12,5 @@ app.listen(port, () => console.log(`IoT service started on port ${port}`));
 app.use(auth);
 app.use(router);
 app.use(error);
+
+new Scheduler();
