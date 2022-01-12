e5b10d45027dbe6778cd1b558e24d96b52a602e8
Frants Mitskun <frants.mitskun@gmail.com>
2022-01-13 00:38:36 +0300

    fixes

--- scheduler/light.schedule.ts
+++ scheduler/light.schedule.ts
@@ -4,39 +4,31 @@ import {userDeviceController} from "../controller";
 
 export default class LightSchedule {
     constructor() {
-        // Пытаться включить подсветку каждую минуту
-        // с 07:50 до 07:59 c понедельника по пятницу
+        // Включать подсветку в 07:55 если к WiFi подключен телефон
         schedule.scheduleJob(
-            '50-59/1 7 * * 1-5',
-            () => {
-                // если хоть одно устройство пользователя
-                // подкл. к локальной сети (актуально)
+            {hour: 7, minute: 55},
+            async () => {
                 if (userDeviceController.isAnyDeviceConnected()) {
-                    tuyaDeviceController.action('ambient', 'on')
+                    await tuyaDeviceController.action('ambient', 'on');
                 }
             }
         );
-        // Пытаться включить лампочку каждую минуту
-        // с 08:00 до 08:05 c понедельника по пятницу
+        // Включать люстру в 08:10 если к WiFi подключен телефон
         schedule.scheduleJob(
-            '0-5/1 8 * * 1-5',
-            () => {
-                // если хоть одно устройство пользователя
-                // подкл. к локальной сети (актуально)
+            {hour: 8, minute: 10},
+            async () => {
                 if (userDeviceController.isAnyDeviceConnected()) {
-                    tuyaDeviceController.action('top', 'on')
+                    await tuyaDeviceController.action('top', 'on');
                 }
             }
         );
 
-        // Выключить свет
-        // в 01:00
+        // Выключать весь свет в 01:00
         schedule.scheduleJob(
             {hour: 1, minute: 0},
             () => tuyaDeviceController.action('all', 'off')
         );
-        // Выключить свет
-        // в 09:30
+        // Выключать весь свет в 09:30
         schedule.scheduleJob(
             {hour: 9, minute: 30},
             () => tuyaDeviceController.action('all', 'off')
