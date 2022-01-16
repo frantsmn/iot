f30fa4d3d687981a4ddead250cb1bfc64ec9b351
Frants Mitskun <frants.mitskun@gmail.com>
2022-01-16 13:10:59 +0300

    исправляет расписание

--- scheduler/light.schedule.ts
+++ scheduler/light.schedule.ts
@@ -4,25 +4,24 @@ import {userDeviceController} from "../controller";
 
 export default class LightSchedule {
     constructor() {
-        // Включать подсветку в 07:55 если к WiFi подключен телефон
+        // Включать люстру в 07:55 если к WiFi подключен телефон
         schedule.scheduleJob(
             {hour: 7, minute: 55, dayOfWeek: [1, 2, 3, 4, 5]},
-            async () => {
-                if (userDeviceController.isAnyDeviceConnected()) {
-                    await tuyaDeviceController.action('ambient', 'on');
-                }
-            }
-        );
-        // Включать люстру в 08:10 если к WiFi подключен телефон
-        const s = schedule.scheduleJob(
-            {hour: 8, minute: 10, dayOfWeek: [1, 2, 3, 4, 5]},
             async () => {
                 if (userDeviceController.isAnyDeviceConnected()) {
                     await tuyaDeviceController.action('top', 'on');
                 }
             }
         );
-        console.log('ni', s.nextInvocation());
+        // Включать подсветку в 08:10 если к WiFi подключен телефон
+        const s = schedule.scheduleJob(
+            {hour: 8, minute: 10, dayOfWeek: [1, 2, 3, 4, 5]},
+            async () => {
+                if (userDeviceController.isAnyDeviceConnected()) {
+                    await tuyaDeviceController.action('ambient', 'on');
+                }
+            }
+        );
 
         // Выключать весь свет в 09:30 (если к WiFi не подключен телефон)
         schedule.scheduleJob(
