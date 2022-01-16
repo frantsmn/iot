38902219822d16d0d780c60036a6dc3893f30c28
Frants Mitskun <frants.mitskun@gmail.com>
2022-01-16 13:08:24 +0300

    исправляет расписание

--- scheduler/light.schedule.ts
+++ scheduler/light.schedule.ts
@@ -6,7 +6,7 @@ export default class LightSchedule {
     constructor() {
         // Включать подсветку в 07:55 если к WiFi подключен телефон
         schedule.scheduleJob(
-            {hour: 7, minute: 55},
+            {hour: 7, minute: 55, dayOfWeek: [1, 2, 3, 4, 5]},
             async () => {
                 if (userDeviceController.isAnyDeviceConnected()) {
                     await tuyaDeviceController.action('ambient', 'on');
@@ -14,24 +14,29 @@ export default class LightSchedule {
             }
         );
         // Включать люстру в 08:10 если к WiFi подключен телефон
-        schedule.scheduleJob(
-            {hour: 8, minute: 10},
+        const s = schedule.scheduleJob(
+            {hour: 8, minute: 10, dayOfWeek: [1, 2, 3, 4, 5]},
             async () => {
                 if (userDeviceController.isAnyDeviceConnected()) {
                     await tuyaDeviceController.action('top', 'on');
                 }
             }
         );
+        console.log('ni', s.nextInvocation());
 
+        // Выключать весь свет в 09:30 (если к WiFi не подключен телефон)
+        schedule.scheduleJob(
+            {hour: 9, minute: 30},
+            async () => {
+                if (userDeviceController.isAnyDeviceConnected() === false) {
+                    await tuyaDeviceController.action('all', 'off');
+                }
+            }
+        );
         // Выключать весь свет в 01:00
         schedule.scheduleJob(
             {hour: 1, minute: 0},
-            () => tuyaDeviceController.action('all', 'off')
-        );
-        // Выключать весь свет в 09:30
-        schedule.scheduleJob(
-            {hour: 9, minute: 30},
-            () => tuyaDeviceController.action('all', 'off')
+            async () => await tuyaDeviceController.action('all', 'off')
         );
     }
 }
