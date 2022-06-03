54921911e134ff87a5821d607cadc8b27d10cac1
Frants Mitskun <frants.mitskun@gmail.com>
2022-06-03 17:30:31 +0300

    hotfix

--- app.ts
+++ app.ts
@@ -18,7 +18,7 @@ app.listen(port, () => log.info(`iot started on port ${port}`));
 app.use(appLogger);
 app.use(express.json());
 app.use(auth);
-app.use(error);
 app.use(device);
 app.use(restart);
 app.use(logs);
+app.use(error);
